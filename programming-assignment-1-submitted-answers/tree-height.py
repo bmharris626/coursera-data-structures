73256048103d93f52586ab830b47e0e7e155cad7
Brandon Harris <bmharris626@gmail.com>
2018-01-15 13:36:38 -0500

    Finished 01-Ex1

--- programming-assignment-1-submitted-answers/tree-height.py
+++ programming-assignment-1-submitted-answers/tree-height.py
@@ -8,14 +8,28 @@ class TreeHeight:
     def read(self):
         self.n = int(sys.stdin.readline())
         self.parent = list(map(int, sys.stdin.readline().split()))
+        self.depths = [None for i in range(self.n)]
 
+    def node_height(self, node):
+        parent = self.parent[node]
+        self.depths[node] = self.depths[parent] + 1
+        children = self.children[node]
+        if children:
+            for child in children:
+                self.node_height(child) 
+    
     def compute_height(self):
         nodes = [list() for i in range(self.n)]
         for child_index in range(self.n):
             parent_index = self.parent[child_index]
             if parent_index == -1: self.root = child_index
             else: nodes[parent_index].append(child_index)
-        return sum(1 for node in nodes if node) + 1
+        self.children = nodes
+        self.depths[self.root] = 1
+        max_height = 0
+        for child in self.children[self.root]:
+            self.node_height(child)
+        return max(self.depths)
 
 def main():
     tree = TreeHeight()
