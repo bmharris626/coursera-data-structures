aeaff5752615f467ccd39dbb303336aa5330ac94
Brandon Harris <bmharris626@gmail.com>
2018-02-21 08:21:06 -0500

    Added Ex-2-03 Answer

--- programming-assignment-2-answers/merging_tables.py
+++ programming-assignment-2-answers/merging_tables.py
@@ -4,28 +4,30 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 lines = list(map(int, sys.stdin.readline().split()))
-rank = [1] * n
 parent = list(range(0, n))
 ans = max(lines)
 
-def getParent(table):
-    # find parent and compress path
-    return parent[table]
+def find(i):
+    while i != parent[i]:
+        i = find(parent[i])
+    return i
 
-def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
-
-    if realDestination == realSource:
-        return False
-
-    # merge two components
-    # use union by rank heuristic 
-    # update ans with the new maximum table size
-    
-    return True
+def merge(i, j):
+    i_id = find(i)
+    j_id = find(j)
+    if i_id == j_id:
+        return lines[i_id]
+    if lines[i_id] > lines[j_id]:
+        parent[j_id] = i_id
+        lines[i_id] += lines[j_id]
+        return lines[i_id]
+    else:
+        parent[i_id] = parent[j_id]
+        lines[j_id] += lines[i_id]
+        return lines[j_id]
 
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
-    merge(destination - 1, source - 1)
+    k = merge(destination - 1, source - 1)
+    if k > ans: ans = k
     print(ans)
-    
