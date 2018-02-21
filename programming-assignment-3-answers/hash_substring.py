e00075b905bdf079f78d4ce9f9d330842a9858f5
Brandon Harris <bmharris626@gmail.com>
2018-02-20 20:24:08 -0500

    Finished Ex4-1

--- programming-assignment-3-answers/hash_substring.py
+++ programming-assignment-3-answers/hash_substring.py
@@ -1,4 +1,5 @@
 # python3
+import random
 
 def read_input():
     return (input().rstrip(), input().rstrip())
@@ -6,13 +7,33 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    return [
-        i 
-        for i in range(len(text) - len(pattern) + 1) 
-        if text[i:i + len(pattern)] == pattern
-    ]
+def PolyHash(S, p, x):
+    hsh = 0
+    for c in reversed(S):
+        hsh = (hsh * x + ord(c)) % p
+    return hsh
+
+def PrecomputeHashes(T, P, p, x):
+    H = [None for i in range(len(T)-len(P)+1)]
+    S = T[len(T)-len(P):len(T)]
+    H[len(T)-len(P)] = PolyHash(S, p, x)
+    y = 1
+    for i in range(1, len(P)+1):
+        y = (y * x) % p
+    for i in range(len(T)-len(P)-1, -1, -1):
+        H[i] = (x*H[i+1] + ord(T[i]) - y * ord(T[i+len(P)])) % p
+    return H
+
+def RubinKarp(P, T):
+    p = 1000000007
+    x = random.randint(1, p-1)
+    result = list()
+    H = PrecomputeHashes(T, P, p, x)
+    pHash = PolyHash(P, p, x)
+    for i in range(len(T) - len(P) +1):
+        if pHash != H[i]: continue
+        if T[i:i+len(P)] == P: result.append(i)
+    return result
 
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
-
+    print_occurrences(RubinKarp(*read_input()))
